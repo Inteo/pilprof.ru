c7a92662a99a508a4b25a6f3793fc7983041b987
BerTepesh <Ber.Tepesh@gmail.com>
2017-07-25 17:02:07 +0300

    Услуги, О производстве

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+  $(".switcher_btn").click(function(){
+    $(this).closest(".switcher_control").find(".switcher_btn").removeClass("active");
+    $(this).addClass("active");
+    var holder = $(this).closest(".switcher").find(".switcher_holder");
+    holder.removeClass("active");
+    holder.filter('[data-switch-hold='+$(this).data("switch-btn")+']').addClass("active");
+    return false;
+  });
 });
